4f0c17b34fa0935f3b105f2924d2b774f168567e
Chao Li <chaoli0394@gmail.com>
2022-04-07 14:39:36 +0900

    B: regression test; revise 02_AN; done

--- 02_RCode/02_AN_SafetyHappinessNDVINTL_v1.R
+++ 02_RCode/02_AN_SafetyHappinessNDVINTL_v1.R
@@ -16,37 +16,71 @@ dataset_used <- dataset_used %>%
   mutate(NTL_log = ifelse(NTL < 0, 0, NTL))
 dataset_used$NTL_log <- log(dataset_used$NTL_log + 1)
 dataset_used$NTL_log %>% hist()
+dataset_used$age2 <- dataset_used$age * dataset_used$age
 
 pdata <- pdata.frame(dataset_used, index = c("ID", "year"))
 
-formula <- live_environment_satefy ~ female + age + self_reported_health + income_indiv + college_no_diploma +
-  bachelor + master + phd + good_for_living + community_attachment + NTL_log
+### safety
+formula_safe <- live_environment_satefy ~ female + age + age2 + self_reported_health +
+  income_indiv + college_no_diploma + bachelor + master + phd  + NTL_log + NDVI
 
 cor(dataset_used %>% dplyr::select(all.vars(formula)) %>% na.omit())
 
-ols <- plm(formula = formula, pdata, effect = "individual", model = "pooling")
-summary(ols)
+ols_safe <- plm(formula = formula_safe, pdata, effect = "individual", model = "pooling")
+summary(ols_safe)
 
-#rem <- plm(formula = formula, pdata, effect = "individual", model = "random")
-#summary
+rem_safe <- plm(formula = formula_safe, pdata, effect = "individual", model = "random")
+summary(rem_safe)
 
-formula_high_stress <- high_stress ~ female + age + self_reported_health + income_indiv + college_no_diploma +
-  bachelor + master + phd + good_for_living + community_attachment + NTL_log
+### good for living
+formula_goodLiving <- good_for_living ~ female + age +age2 + self_reported_health +
+  income_indiv + college_no_diploma + bachelor + master + phd  + NTL_log + NDVI
+
+ols_goodLiving <- plm(formula = formula_goodLiving, pdata, effect = "individual", model = "pooling")
+summary(ols_goodLiving)
+
+rem_goodLiving <- plm(formula = formula_goodLiving, pdata, effect = "individual", model = "random")
+summary(rem_goodLiving)
+
+### community attachment
+formula_attach <- community_attachment ~ female + age + age2 + self_reported_health +
+  income_indiv + college_no_diploma + bachelor + master + phd  + NTL_log +NDVI
+
+ols_attach <- plm(formula = formula_attach, pdata, effect = "individual", model = "pooling")
+summary(ols_attach)
+
+rem_attach <- plm(formula = formula_attach, pdata, effect = "individual", model = "random")
+summary(rem_attach)
+
+### high stress
+formula_high_stress <- high_stress ~ female + age + age2 + self_reported_health +
+  income_indiv + college_no_diploma + bachelor + master + phd + NTL_log + NDVI
+
 ols_high_stress <- plm(formula = formula_high_stress, pdata, effect = "individual", model = "pooling")
 summary(ols_high_stress)
 
-formula_low_stress <- low_stress ~ female + age + self_reported_health + income_indiv + college_no_diploma +
-  bachelor + master + phd + good_for_living + community_attachment + NTL_log
+rem_high_stress <- plm(formula = formula_high_stress, pdata, effect = "individual", model = "random")
+summary(rem_high_stress)
+
+### low stress
+formula_low_stress <- low_stress ~ female + age + age2 + self_reported_health +
+  income_indiv + college_no_diploma + bachelor + master + phd +  NTL_log + NDVI
 ols_low_stress <- plm(formula = formula_low_stress, pdata, effect = "individual", model = "pooling")
 summary(ols_low_stress)
 
-formula_ha <- overall_happiness ~ live_environment_satefy + female + age + 
-  self_reported_health + income_indiv + college_no_diploma +
-  bachelor + master + phd + good_for_living + community_attachment + NDVI 
+rem_low_stress <- plm(formula = formula_low_stress, pdata, effect = "individual", model = "random")
+summary(rem_low_stress)
+
+
+
+formula_ha <- overall_happiness ~ live_environment_satefy + 
+  good_for_living + community_attachment + high_stress + low_stress +
+  female + age + age2 + self_reported_health + income_indiv + college_no_diploma +
+  bachelor + master + phd +  NDVI + NTL
 
 ols_ha <- plm(formula = formula_ha, pdata, effect = "individual", model = "pooling")
 summary(ols_ha)
 
-#rem <- plm(formula = formula, pdata, effect = "individual", model = "random")
-#summary(rem)
+rem_ha <- plm(formula = formula_ha, pdata, effect = "individual", model = "random")
+summary(rem_ha)
 
