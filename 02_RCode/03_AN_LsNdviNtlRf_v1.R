9ed14d15bc9f819792d6077fe00fc14a4992d521
Chao Li <chaoli0394@gmail.com>
2022-04-22 17:12:02 +0900

    C: check the model; every thing good; done

--- 02_RCode/03_AN_LsNdviNtlRf_v1.R
+++ 02_RCode/03_AN_LsNdviNtlRf_v1.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(DALEX)
 library(doSNOW)
 library(tcltk)
+library(pdp)
 
 load("01_Data/04_dataset_used.RData")
 dataset_used <- dataset_used %>%
@@ -33,7 +34,7 @@ dataset_used.rf <- dataset_used %>% dplyr::select(all.vars(formula_LS), student:
 ### since the there is 24 predictors, we select 24/3 ~ 8
 
 # do SNOW
-cl <- makeSOCKcluster(14)
+cl <- makeSOCKcluster(15)
 registerDoSNOW(cl)
 
 ntasks <- 100
@@ -42,11 +43,51 @@ progress <- function(n) setTkProgressBar(pb, n)
 opts <- list(progress=progress)
 
 data.rf.24 <- 
-  foreach(ntree = rep(10, ntasks), .combine = combine,
+  foreach(ntree = rep(10, ntasks), .combine = randomForest::combine,
           .multicombine=TRUE, .packages='randomForest',
           .options.snow=opts) %dopar% {
             randomForest(overall_LS ~., data = dataset_used.rf, 
-                         na.action = na.omit, ntree = 1000,
+                         na.action = na.omit, ntree = ntree,
                          importance = T, mtry = 8)
           }
+stopCluster(cl)
 # do SNOW
+
+save(data.rf.24, file = "03_Results/00_data.rf.24.RData", version = 2)
+
+plot(data.rf.24)
+importance(data.rf.24)
+varImpPlot(data.rf.24)
+print(data.rf.24)
+
+### calculate loss function
+loss_root_mean_square(dataset_used.rf$overall_LS, yhat(data.rf.24, dataset_used.rf))
+
+### unified the model
+explainer_data.rf.24 = explain(data.rf.24, data = dataset_used.rf, y = dataset_used.rf$overall_LS)
+diag_data.rf.24 <- model_diagnostics(explainer_data.rf.24)
+plot(diag_data.rf.24)
+plot(diag_data.rf.24, variable = "y", yvariable = "residuals")
+hist(dataset_used.rf$overall_LS, breaks = rep(0:5, 1))
+
+### model information
+model_info(data.rf.24)
+
+### Dataset Level Variable Importance as Change in Loss Function after Variable Permutations
+data.rf.24_aps <- model_parts(explainer_data.rf.24, type = "raw")
+head(data.rf.24_aps, 10)
+plot(data.rf.24_aps)
+
+### model performance
+model_performance_data.rf.24 <- model_performance(explainer_data.rf.24)
+model_performance_data.rf.24
+plot(model_performance_data.rf.24)
+
+lm(overall_LS ~., data = dataset_used.rf) %>% summary()
+
+### test the line
+### Dataset Level Variable Profile as Partial Dependence or Accumulated Local Dependence Explanations
+model_profile_data.rf.24 <- model_profile(explainer_data.rf.24)
+plot(model_profile_data.rf.24, variables = "NDVI")
+plot(model_profile_data.rf.24, variables = "NTL_log")
+plot(model_profile_data.rf.24, variables = "income_indiv")
