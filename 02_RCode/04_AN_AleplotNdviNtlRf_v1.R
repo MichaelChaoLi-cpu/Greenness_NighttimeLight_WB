44f0a1c909d45b9a212ecc60681819ab9577ffac
Chao Li <chaoli0394@gmail.com>
2022-05-24 10:50:35 +0900

    A: begin to calculate pdp; revise 04_AN 6 cluster unable use cluster; done

--- 02_RCode/04_AN_AleplotNdviNtlRf_v1.R
+++ 02_RCode/04_AN_AleplotNdviNtlRf_v1.R
@@ -11,7 +11,7 @@ library(tcltk)
 library(pdp)
 
 load("03_Results/00_data.rf.24.RData")
-load("01_Data/06_dataset,rf24.RData")
+load("01_Data/06_dataset.rf24.RData")
 ALE.2.rf24 = ALEPlot(dataset_used.rf[,2:25], data.rf.24, pred.fun = yhat, 
                 J = c('NDVI', 'NTL_log'), K = 100, NA.plot = T)
 image(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values, xlab = "NDVI", ylab = "NTL")
@@ -19,18 +19,22 @@ contour(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values,
 
 ALE.2.rf24.inc_NTL = ALEPlot(dataset_used.rf[,2:25], data.rf.24, pred.fun = yhat, 
                      J = c('income_indiv', 'NTL_log'), K = 100, NA.plot = T)
-#image(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values, xlab = "NDVI", ylab = "NTL")
-#contour(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values, add = T, drawlabels = T)
+image(ALE.2.rf24.inc_NTL$x.values[[1]], ALE.2.rf24.inc_NTL$x.values[[2]],
+      ALE.2.rf24.inc_NTL$f.values, xlab = "NDVI", ylab = "NTL")
+contour(ALE.2.rf24.inc_NTL$x.values[[1]], ALE.2.rf24.inc_NTL$x.values[[2]],
+        ALE.2.rf24.inc_NTL$f.values, add = T, drawlabels = T)
 
 ALE.2.rf24.inc_NDVI = ALEPlot(dataset_used.rf[,2:25], data.rf.24, pred.fun = yhat, 
                              J = c('income_indiv', 'NDVI'), K = 100, NA.plot = T)
-#image(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values, xlab = "NDVI", ylab = "NTL")
-#contour(ALE.2.rf24$x.values[[1]], ALE.2.rf24$x.values[[2]], ALE.2.rf24$f.values, add = T, drawlabels = T)
+image(ALE.2.rf24.inc_NDVI$x.values[[1]], ALE.2.rf24.inc_NDVI$x.values[[2]],
+      ALE.2.rf24.inc_NDVI$f.values, xlab = "NDVI", ylab = "NTL")
+contour(ALE.2.rf24.inc_NDVI$x.values[[1]], ALE.2.rf24.inc_NDVI$x.values[[2]],
+        ALE.2.rf24.inc_NDVI$f.values, add = T, drawlabels = T)
 
-save(ALE.2.rf24, file = "03_Results/02_ALE.2.rf24.RData")
+save(ALE.2.rf24, ALE.2.rf24.inc_NTL, ALE.2.rf24.inc_NDVI, file = "03_Results/02_ALE.2.rf24.RData")
 
 #### pdp
-cl <- makeSOCKcluster(14)
+cl <- makeSOCKcluster(6)
 registerDoSNOW(cl)
 getDoParWorkers()
 
