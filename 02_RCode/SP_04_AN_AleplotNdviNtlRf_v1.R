a3510fbe8adab0a89c25f76138a97959fd9aa663
Chao Li <chaoli0394@gmail.com>
2022-06-02 10:09:47 +0900

    O: reduce resolution to 1000 and add the report functions; revise SP_04_AN; done

--- 02_RCode/SP_04_AN_AleplotNdviNtlRf_v1.R
+++ 02_RCode/SP_04_AN_AleplotNdviNtlRf_v1.R
@@ -5,8 +5,9 @@
 library(foreach)
 library(randomForest)
 library(tidyverse)
-library(doParallel)
+library(doSNOW)
 library(pdp)
+
 
 
 load("/home/usr6/q70176a/DP15/01_Data/06_dataset.rf24.RData")
@@ -14,40 +15,54 @@ load("/home/usr6/q70176a/DP15/01_Data/06_dataset.rf24.RData")
 #data.rf.24 <- randomForest(overall_LS ~., data = dataset_used.rf, na.action = na.omit, ntree = 1000, 
 #                           importance = T, mtry = 8)
 ### since the there is 24 predictors, we select 24/3 ~ 8
+Sys.time()
 cat("Here, Random forest \n")
 
 # do parallel
-cl <- makeCluster(100)
-registerDoParallel(cl)
-getDoParWorkers()
-
-ntasks <- 10
-
-data.rf.24 <- 
-  foreach(ntree = rep(100, ntasks), .combine = randomForest::combine,
-          .multicombine=TRUE, .packages='randomForest') %dopar% {
-            randomForest(overall_LS ~., data = dataset_used.rf, 
-                         na.action = na.omit, ntree = ntree,
-                         importance = T, mtry = 8)
-          }
-stopCluster(cl)
-# do SNOW
-
-save(data.rf.24, file = "/home/usr6/q70176a/DP15/03_Results/00_data.rf.24.SP.RData", version = 2)
+run <- F
+if(run){
+  cl <- makeSOCKcluster(100)
+  registerDoSNOW(cl)
+  getDoParWorkers()
+  
+  ntasks <- 10
+  
+  data.rf.24 <- 
+    foreach(ntree = rep(100, ntasks), .combine = randomForest::combine,
+            .multicombine=TRUE, .packages='randomForest') %dopar% {
+              randomForest(overall_LS ~., data = dataset_used.rf, 
+                           na.action = na.omit, ntree = ntree,
+                           importance = T, mtry = 8)
+            }
+  stopCluster(cl)
+  # do SNOW
+  
+  save(data.rf.24, file = "/home/usr6/q70176a/DP15/03_Results/00_data.rf.24.SP.RData", version = 2)
+} else {
+  load("/home/usr6/q70176a/DP15/03_Results/00_data.rf.24.SP.RData")
+  Sys.time()
+}
 cat("Here, we have saved the rf model\n")
 
 cat("Here, we are, go to pdp\n")
 
 #### pdp
-cl <- makeCluster(36)
-registerDoParallel(cl)
+cl <- makeSOCKcluster(36)
+registerDoSNOW(cl)
 getDoParWorkers()
+progress <- function(n) {
+  if(n%%50 == 0){
+    cat(sprintf("task %d is complete\n", n)) 
+  }
+}
+opts <- list(progress = progress)
 
 Sys.time()
 pdp.rf24.NDVI <- pdp::partial(data.rf.24, pred.var = "NDVI",
-                               grid.resolution = 5000,
+                               grid.resolution = 1000,
                                plot = F, rug = T, parallel = T,
-                               paropts = list(.packages = "randomForest"))
+                               paropts = list(.packages = "randomForest",
+                                              .options.snow = opts))
 Sys.time()
 
 stopCluster(cl)
@@ -55,15 +70,22 @@ save(pdp.rf24.NDVI, file = "/home/usr6/q70176a/DP15/03_Results/03_data.rf.24.PDP
 
 cat("Here, we are, go to second pdp\n")
 
-cl <- makeCluster(36)
-registerDoParallel(cl)
+cl <- makeSOCKcluster(36)
+registerDoSNOW(cl)
 getDoParWorkers()
+progress <- function(n) {
+  if(n%%50 == 0){
+    cat(sprintf("task %d is complete\n", n)) 
+  }
+}
+opts <- list(progress = progress)
 
 Sys.time()
 pdp.rf24.NTL_log <- pdp::partial(data.rf.24, pred.var = "NTL_log",
-                         grid.resolution = 5000,
+                         grid.resolution = 1000,
                          plot = F, rug = T, parallel = T,
-                         paropts = list(.packages = "randomForest"))
+                         paropts = list(.packages = "randomForest",
+                                        .options.snow = opts))
 Sys.time()
 
 stopCluster(cl)
