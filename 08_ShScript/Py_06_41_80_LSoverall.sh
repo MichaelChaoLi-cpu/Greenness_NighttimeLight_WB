ec0134ea30c21f709e5c95b570c18c9a80d535b1
Chao Li <chaoli0394@gmail.com>
2022-12-21 15:43:46 +0900

    B: upload linux script; create Py_06*.sh; done

---
+++ 08_ShScript/Py_06_41_80_LSoverall.sh
@@ -0,0 +1,1 @@
+for RUNNING in 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80; do python "/mnt/d/OneDrive - Kyushu University/15_Article/03_RStudio/06_PyCode/06_AN_getShapPerThousand_v1.py" $RUNNING LSoverall; done
