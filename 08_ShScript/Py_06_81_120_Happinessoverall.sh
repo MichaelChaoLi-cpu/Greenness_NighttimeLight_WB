357b053b604fa1eeab46f0db9102d54120080fef
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-01-25 11:28:40 +0900

    A: update linux script for overall happiness from 81 to 200; upload 2 .sh; done

---
+++ 08_ShScript/Py_06_81_120_Happinessoverall.sh
@@ -0,0 +1,1 @@
+for RUNNING in 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109 110 111 112 113 114 115 116 117 118 119 120; do python "/mnt/d/OneDrive - Kyushu University/15_Article/03_RStudio/06_PyCode/06_AN_getShapPerThousand_v1.py" $RUNNING Happinessoverall; done
