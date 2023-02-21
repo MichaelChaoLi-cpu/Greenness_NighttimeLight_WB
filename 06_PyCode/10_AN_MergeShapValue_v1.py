0d83a676dd775ad59fbde40db354d691e2aeec83
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-02-21 16:26:23 +0900

    C: plot the relationship between SHAP and real value, NTL are more like linear; revise 10_AN and 11_VI; done

--- 06_PyCode/10_AN_MergeShapValue_v1.py
+++ 06_PyCode/10_AN_MergeShapValue_v1.py
@@ -35,10 +35,21 @@ def runLocallyOrRemotely(Locally_Or_Remotely):
         repo_result_location = "/Users/lichao/Library/CloudStorage/OneDrive-KyushuUniversity/02_Article/15_RStudio/07_PyResults/"
     return repo_location, repo_result_location
 
-def mergeShapDataset():
+def mergeShapDatasetLsoverall():
     csv_read_list = []
     for order_num in list(range(1, 385, 1)):
         csv_name = REPO_RESULT_LOCATION + "shap_thousand_" + str(order_num) + ".csv"
+        csv_read = pd.read_csv(csv_name, index_col=0)
+        csv_read_list.append(csv_read)
+    shap_value_merge = pd.concat(csv_read_list, axis=0)
+    shap_value_merge.reset_index(inplace = True)
+    shap_value_merge.drop(columns='index', inplace=True)
+    return shap_value_merge
+
+def mergeShapDataset(Variable_Of_Interest):
+    csv_read_list = []
+    for order_num in list(range(1, 385, 1)):
+        csv_name = REPO_RESULT_LOCATION + "shap_thousand_" + str(order_num) + "_" + Variable_Of_Interest + ".csv"
         csv_read = pd.read_csv(csv_name, index_col=0)
         csv_read_list.append(csv_read)
     shap_value_merge = pd.concat(csv_read_list, axis=0)
@@ -53,12 +64,19 @@ def getXwithShap(Merged_Shap_Value, Variable_Of_Interest):
     X_colname = X.columns
     shap_colnames = X_colname[1:] + "_shap"
     Merged_Shap_Value.columns = shap_colnames
-    dataset_to_analysis = pd.concat([X, Merged_Shap_Value], axis=1)
+    data_location = REPO_LOCATION + "01_Data/*_y_" + Variable_Of_Interest + "*.csv"
+    data_location = glob(data_location)[0]
+    y = pd.read_csv(data_location)
+    y = y.iloc[:,[1]]
+    dataset_to_analysis = pd.concat([X, y, Merged_Shap_Value], axis=1)
     return dataset_to_analysis
 
 def run(Variable_Of_Interest):
     REPO_LOCATION, REPO_RESULT_LOCATION = runLocallyOrRemotely('y')
-    Merged_Shap_Value = mergeShapDataset()
+    if Variable_Of_Interest == 'LSoverall':
+        Merged_Shap_Value = mergeShapDatasetLsoverall()
+    else:
+        Merged_Shap_Value = mergeShapDataset(Variable_Of_Interest)
     Dataset_To_Analysis = getXwithShap(Merged_Shap_Value, Variable_Of_Interest)
     Dataset_To_Analysis.to_csv(REPO_RESULT_LOCATION + "00_mergedXSHAP_"+ Variable_Of_Interest +".csv", index = False)
     return None
