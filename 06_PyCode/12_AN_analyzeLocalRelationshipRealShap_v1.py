ed3cacef252bdec83e004f8dd82d84f0278227e3
MichaelChaoLi-cpu <chaoli0394@gmail.com>
2023-02-22 16:19:14 +0900

    C: revise the code run for the different output variables; revise 12_AN; done

--- 06_PyCode/12_AN_analyzeLocalRelationshipRealShap_v1.py
+++ 06_PyCode/12_AN_analyzeLocalRelationshipRealShap_v1.py
@@ -163,27 +163,65 @@ def singleCoefficientBetweenLandCoverAndItsShapGw(neighbors, variable_name,
     return [coef, intercept, t_coef, t_interc]
 
 def getMergeSHAPresult(Variable_Of_Interest):
-    result = pd.read_csv(REPO_RESULT_LOCATION + "00_mergedXSHAP_" + Variable_Of_Interest+ ".csv", index_col=0)
+    result = pd.read_csv(REPO_RESULT_LOCATION + "00_mergedXSHAP_" + Variable_Of_Interest + ".csv", index_col=0)
     return result
 
+def keepSignificantValue(Spatial_Coefficient_Df):
+    Spatial_Coefficient_Df.fillna(0, inplace=True)
+    Spatial_Coefficient_Df.loc[abs(Spatial_Coefficient_Df[Spatial_Coefficient_Df.columns.values[2]]) < 1.64, 
+                               Spatial_Coefficient_Df.columns.values[0]] = 0
+    Spatial_Coefficient_Df.loc[abs(Spatial_Coefficient_Df[Spatial_Coefficient_Df.columns.values[3]]) < 1.64,
+                               Spatial_Coefficient_Df.columns.values[1]] = 0
+    return Spatial_Coefficient_Df
 
+def calculateMonetaryValue(Interest_Coeff_Df, Income_Coeff_Df, Mv_Name):
+    Interest_Coeff_Df[Mv_Name] = Interest_Coeff_Df[Interest_Coeff_Df.columns.values[0]]/Income_Coeff_Df[Income_Coeff_Df.columns.values[0]]
+    Interest_Coeff_Df.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
+    return Interest_Coeff_Df
 
+def getLonLatId(X):
+    lon_lat_df = X.reset_index()
+    lon_lat_df = lon_lat_df[['index', 'lon', 'lat']]
+    return lon_lat_df
+
+Output_Variable = 'LSoverall'
 REPO_LOCATION, REPO_RESULT_LOCATION = runLocallyOrRemotely('y')
-model, X, y = makeModel('LSoverall')
 
-X_split_array, Y_split_array = XYSplit(model)
-leftRightBoundary = findBoundaryArray(X_split_array, X.iloc[:,2])
-upDownBoundary = findBoundaryArray(Y_split_array, X.iloc[:,1])
-neighborList = buildNeighborList(X, leftRightBoundary, upDownBoundary)
-dump(leftRightBoundary, REPO_RESULT_LOCATION + "05_leftRightBoundary.joblib")
-dump(upDownBoundary, REPO_RESULT_LOCATION + "06_upDownBoundary.joblib")
-dump(neighborList, REPO_RESULT_LOCATION + "07_neighborList.joblib")
+def run(Output_Variable):
+    model, X, y = makeModel(Output_Variable)
+    
+    X_split_array, Y_split_array = XYSplit(model)
+    leftRightBoundary = findBoundaryArray(X_split_array, X.iloc[:,2])
+    upDownBoundary = findBoundaryArray(Y_split_array, X.iloc[:,1])
+    neighborList = buildNeighborList(X, leftRightBoundary, upDownBoundary)
+    dump(leftRightBoundary, REPO_RESULT_LOCATION + "05_leftRightBoundary_" + Output_Variable +".joblib")
+    dump(upDownBoundary, REPO_RESULT_LOCATION + "06_upDownBoundary_" + Output_Variable +".joblib")
+    dump(neighborList, REPO_RESULT_LOCATION + "07_neighborList_" + Output_Variable +".joblib")
+    
+    result = getMergeSHAPresult(Output_Variable)
+    NDVI_spatialcoefficient = SpatialCoefficientBetweenLandCoverAndItsShapGw('NDVI', result, neighborList)
+    NTL_spatialcoefficient = SpatialCoefficientBetweenLandCoverAndItsShapGw('NTL', result, neighborList)
+    income_spatialcoefficient = SpatialCoefficientBetweenLandCoverAndItsShapGw('income_indiv', result, neighborList)
+    dump(NDVI_spatialcoefficient, REPO_RESULT_LOCATION + "08_NDVI_spatialcoefficient_" + Output_Variable +".joblib")
+    dump(NTL_spatialcoefficient, REPO_RESULT_LOCATION + "09_NTL_spatialcoefficient_" + Output_Variable +".joblib")
+    dump(income_spatialcoefficient, REPO_RESULT_LOCATION + "10_income_spatialcoefficient_" + Output_Variable +".joblib")
+    
+    NDVI_spatialcoefficient_sig = keepSignificantValue(NDVI_spatialcoefficient)
+    NTL_spatialcoefficient_sig = keepSignificantValue(NTL_spatialcoefficient)
+    income_spatialcoefficient_sig = keepSignificantValue(income_spatialcoefficient)
+    
+    NDVI_MV = calculateMonetaryValue(NDVI_spatialcoefficient_sig, income_spatialcoefficient_sig, 'NDVI_MV')
+    NTL_MV = calculateMonetaryValue(NTL_spatialcoefficient_sig, income_spatialcoefficient_sig, 'NTL_MV')
+    
+    lon_lat_df = getLonLatId(X)
+    MV_Location_Df = pd.concat([lon_lat_df, NDVI_MV, NTL_MV], axis=1)
+    dump(MV_Location_Df, REPO_RESULT_LOCATION + "11_MV_Location_Df_" + Output_Variable +".joblib")
+    return None
 
-result = getMergeSHAPresult('LSoverall')
-NDVI_spatialcoefficient = SpatialCoefficientBetweenLandCoverAndItsShapGw('NDVI', result, neighborList)
-NTL_spatialcoefficient = SpatialCoefficientBetweenLandCoverAndItsShapGw('NTL', result, neighborList)
-dump(NDVI_spatialcoefficient, REPO_RESULT_LOCATION + "08_NDVI_spatialcoefficient.joblib")
-dump(NTL_spatialcoefficient, REPO_RESULT_LOCATION + "09_NTL_spatialcoefficient.joblib")
+run('LSoverall')
+run('LSrelative')
+run('Happinessoverall')
+run('Happinessrelative')
 
 """
 from joblib import load
